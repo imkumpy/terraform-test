63136687a2371aa23c031b6c5306af508d7b03d8
ImKumpy <lebergarrett@gmail.com>
2020-10-23 16:43:56 -0600

    Added simple variable use

--- example.tf
+++ example.tf
@@ -15,7 +15,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0dba2cb6798deb6d8"
   instance_type = "t2.micro"
-  key_name      = "kumpdev-rsa"
+  key_name      = var.aws_key_pair
 
   tags = {
     Name = "example",
